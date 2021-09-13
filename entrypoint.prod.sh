5760476874bd17e639ff17994bfe20f01798d232
lan666as <4zidan@gmail.com>
2021-09-13 13:35:22 +0700

    fix docker-compose

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -18,4 +18,6 @@
 # echo "from helios_auth.models import User; User.objects.create(user_type='password',user_id='${ADMIN_EMAIL}', info={'name':'${ADMIN_NAME}','password':'${ADMIN_PASS}'}, admin_p=True)"
 # echo "from helios_auth.models import User; User.objects.create(user_type='google',user_id='${ADMIN_EMAIL}', info={'name':'${ADMIN_NAME}'}, admin_p=True)" | python manage.py shell
 
+gunicorn wsgi:application -b 0.0.0.0:${GUNICORN_PORT} -w ${GUNICORN_WORKERS}
+
 exec "$@"
